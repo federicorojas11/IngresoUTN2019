afb7cb506ceb82e5f745f51ad1c5b29f24759430
federicorojas11 <47337947+federicorojas11@users.noreply.github.com>
2019-02-11 22:26:48 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,19 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad = parseInt (document.getElementById("edad").value);
+ 
+if (edad >= 18)
+
+{
+     alert ("Usted es mayor de edad");
+}
 
 
-}//FIN DE LA FUNCIÓN
+
+
+
+
+}//FIN DE LA FUNCIÓ
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,24 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad; 
+
+
+edad = parseInt (document.getElementById("edad").value);
+
+
+if (edad >= 13);
+ {
+ if (edad <= 17);
+  {
+   alert ("Usted es adolescente.")   ;
+  }
+
+ }
+
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
